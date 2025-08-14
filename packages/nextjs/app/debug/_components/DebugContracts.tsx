6bc633b781ef45c5fd70c9df2eae1853cf22fcf8
ParmarHet <het2122005@gmail.com>
2025-08-14 04:22:32 +0000

    commited succesfully..

--- packages/nextjs/app/debug/_components/DebugContracts.tsx
+++ packages/nextjs/app/debug/_components/DebugContracts.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { IVendingMachine } from "./IVendingMachine";
 import { ethers } from "ethers";
 
-const contractAddress = "0x4f6e1bc44ee6f1dd0951b8e5bde4764055918055"; // Get this from run-dev-node.sh output
+const contractAddress="0x232006f2fb38340fd00530cb65ead803b76daedc"; // Get this from run-dev-node.sh output
 const provider = new ethers.JsonRpcProvider(process.env.NEXT_PUBLIC_RPC_URL || "");
 const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY || "";
 const signer = new ethers.Wallet(privateKey, provider);
